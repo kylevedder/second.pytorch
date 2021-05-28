52f8f2c0e4b919120f8487e9c59921df202d5f2a
Kyle Vedder <kyle.c.vedder@gmail.com>
2021-05-28 00:52:02 -0400

    Added run scripts

---
+++ docker_slurm_train_kitti.sh
@@ -0,0 +1,15 @@
+ #!/bin/bash
+srun --gpus=1\
+ --mem-per-gpu=32G\
+ --cpus-per-gpu=16\
+ --qos=eaton-high\
+ --container-mounts=/Datasets:/Datasets,/home/kvedder/code/second.pytorch:/second.pytorch\
+ --container-image=docker-registry.grasp.cluster#second-pytorch-sparse\
+ --time=24:00:00\
+ --partition=eaton-compute\
+ bash -c "nvidia-smi && python ./pytorch/train.py train --config_path=./configs/pointpillars/car/xyres_16_sparse.config --model_dir=./model_dir_car_16_sparse/"
+# -w node-3090-1\
+# --pty\
+# rl_algs/LPG-FTW/experiments/habitat_pgftw.py
+#create_data.py nuscenes_data_prep ROOT_PATH VERSION DATASET_NAME <flags>
+#  optional flags:        --max_sweeps
