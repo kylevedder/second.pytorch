655c6e219d8c45fec96f7f5138c23c6d64c50854
Kyle Vedder <kyle.c.vedder@gmail.com>
2021-07-01 15:50:04 -0400

    Moved output to new folder

--- docker_slurm_train_kitti_sparse_me3.sh
+++ docker_slurm_train_kitti_sparse_me3.sh
@@ -7,7 +7,7 @@ srun --gpus=1\
  --container-image=docker-registry.grasp.cluster#second-pytorch-sparse\
  --time=36:00:00\
  --partition=eaton-compute\
- bash -c "nvidia-smi && python ./pytorch/train.py train --config_path=./configs/pointpillars/car/xyres_16_sparse_me3.config --model_dir=./model_dir_car_16_sparse_me3/"
+ bash -c "nvidia-smi && python ./pytorch/train.py train --config_path=./configs/pointpillars/car/xyres_16_sparse_me3.config --model_dir=./model_dir_car_16_sparse_me3_higher_learning/"
 # -w node-3090-1\
 # --pty\
 # rl_algs/LPG-FTW/experiments/habitat_pgftw.py
